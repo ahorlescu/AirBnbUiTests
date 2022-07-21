6024dd909872574db91c7c0f8bddb69367de76a0
ahorlescu <82826363+ahorlescu@users.noreply.github.com>
2022-07-21 16:45:15 +0300

    Initial commit

---
+++ AirBnbUiTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AirBnbUiTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AirBnbUiTests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a6280fcd-f341-43ae-84f5-6c5cc506a053")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ AirBnbUiTests/WebPage/AirBnbPage.cs
@@ -0,0 +1,29 @@
+﻿namespace AirBnbUiTests.WebPage
+{
+    using System;
+    using OpenQA.Selenium;
+    using OpenQA.Selenium.Support.UI;
+    using SeleniumExtras.PageObjects;
+
+    public class AirBnbPage
+    {
+        #region PageConstructor
+        String test_url = "https://www.airbnb.com/";
+
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+        public AirBnbPage(IWebDriver driver)
+        {
+            this.driver = driver;
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            PageFactory.InitElements(driver, this);
+        }
+        #endregion
+
+        #region Elements
+        [FindsBy(How = How.CssSelector, Using = "button.f19g2zq0 ")]
+        private IWebElement btnAnywere;
+        #endregion
+    }
+}
