37f908217e1eb7ece81247cdbda67a9f64dd557b
ahorlescu <82826363+ahorlescu@users.noreply.github.com>
2022-07-23 15:21:14 +0300

    first commit

---
+++ AirBnbUiTests/Steps/AirBnbSteps.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using TechTalk.SpecFlow;
+using System.Xml;
+using FrameworkBase;
+using AirBnbUiTests.WebPage;
+using OpenQA.Selenium;
+using FrameworkBase.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace AirBnbUiTests.Steps
+{
+    [TestFixture]
+    public class AirBnbSteps
+    {
+        IWebDriver driver;
+        AirBnbPage page;
+        Browser browser;
+        ContainerProvider provider;
+
+        [SetUp]
+        public void Init()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Cookies.DeleteAllCookies();
+
+            page = new AirBnbPage(browser, provider, driver);
+        }
+
+        [Test]
+        public void ValidateMessageIsDisplayedWhenUserEntersTextAndClickShowMessage()
+        {
+            
+
+        }
+    }
+}
--- AirBnbUiTests/WebPage/AirBnbPage.cs
+++ AirBnbUiTests/WebPage/AirBnbPage.cs
@@ -1,29 +1,49 @@
 ﻿namespace AirBnbUiTests.WebPage
 {
     using System;
+    using FrameworkBase.Selenium;
     using OpenQA.Selenium;
     using OpenQA.Selenium.Support.UI;
     using SeleniumExtras.PageObjects;
 
-    public class AirBnbPage
+    public class AirBnbPage : BaseTablePage
     {
         #region PageConstructor
-        String test_url = "https://www.airbnb.com/";
+        String url = "https://www.airbnb.com/";
 
         private IWebDriver driver;
-        private WebDriverWait wait;
 
-        public AirBnbPage(IWebDriver driver)
+        public AirBnbPage(Browser browser, ContainerProvider provider, IWebDriver driver)
+        : base(browser, provider)
         {
             this.driver = driver;
-            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
             PageFactory.InitElements(driver, this);
         }
         #endregion
 
         #region Elements
-        [FindsBy(How = How.CssSelector, Using = "button.f19g2zq0 ")]
-        private IWebElement btnAnywere;
+        [FindsBy(How = How.CssSelector, Using = "button.f19g2zq0")]
+        private IWebElement btnAnywhere;
+
+        [FindsBy(How = How.Id, Using = "bigsearch-query-location-input")]
+        private IWebElement inputWhere;
+
+        [FindsBy(How = How.Id, Using = "bigsearch-query-location-listbox")]
+        private IWebElement ddlWhere;
+        #endregion
+
+        #region Actions
+        public void GoToPage()
+        {
+            driver.Navigate().GoToUrl(url);
+        }
+
+        public void SelectDestination(string country)
+        {
+            DropDown.SelectItemFromFieldByValue(ddlWhere, country);
+        }
+
+        public void 
         #endregion
     }
 }
