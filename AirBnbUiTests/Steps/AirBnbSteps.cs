a7ed793f29addbeebc2bb67a8e7539bd095b5b2b
ahorlescu <82826363+ahorlescu@users.noreply.github.com>
2022-07-24 19:09:48 +0300

    final test

--- AirBnbUiTests/Steps/AirBnbSteps.cs
+++ AirBnbUiTests/Steps/AirBnbSteps.cs
@@ -3,6 +3,7 @@ using AirBnbUiTests.WebPage;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System.Threading;
+using System;
 
 namespace AirBnbUiTests.Steps
 {
@@ -24,12 +25,36 @@ namespace AirBnbUiTests.Steps
         [Test]
         public void ValidateDestination()
         {
+            #region Navigate to page
             page.GoToPage();
             Thread.Sleep(5000);
+            #endregion
+            #region Flow A and B
             page.ClickAnywhereButton();
             page.CheckStaysIsSelected();
             page.EnterDestination("Spain");
+            Thread.Sleep(5000);
             page.SelectDestination();
+            page.SelectCheckInDate();
+            page.SelectCheckOutDate();
+            var expectedCheckInDate = DateTime.UtcNow.Day.ToString();
+            var expectedCheckOutDate = DateTime.UtcNow.AddDays(4).Day.ToString();
+            var actualcheckInDate = page.GetCheckInValue();
+            var actualcheckOutDate = page.GetCheckOutValue();
+            StringAssert.Contains(expectedCheckInDate, actualcheckInDate, "The interval is not selected");
+            StringAssert.Contains(expectedCheckOutDate, actualcheckOutDate, "The interval is not selected");
+            #endregion
+            #region Flow C, D and E
+            page.ClickFlexibleButton();
+            page.ClickWeekendButton();
+            var whenTabValue = page.GetWhenTabValue();
+            StringAssert.AreEqualIgnoringCase("Any Weekend", whenTabValue, "Period is not Any Weekend");
+            page.ClickChooseDatesButton();
+            var checkInDate = page.GetCheckInValue();
+            var checkOutDate = page.GetCheckOutValue();
+            StringAssert.Contains(expectedCheckInDate, checkInDate, "The interval is persisted");
+            StringAssert.Contains(expectedCheckOutDate, checkOutDate, "The interval is persisted");
+            #endregion
         }
 
         [TearDown]
--- AirBnbUiTests/WebPage/AirBnbPage.cs
+++ AirBnbUiTests/WebPage/AirBnbPage.cs
@@ -1,10 +1,13 @@
 ﻿namespace AirBnbUiTests.WebPage
 {
     using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using FrameworkBase.Selenium;
     using OpenQA.Selenium;
     using SeleniumExtras.PageObjects;
 
-    public class AirBnbPage 
+    public class AirBnbPage
     {
         #region PageConstructor
         String url = "https://www.airbnb.com/";
@@ -23,6 +26,15 @@
         [FindsBy(How = How.CssSelector, Using = "button.f19g2zq0")]
         private IWebElement btnAnywhere;
 
+        [FindsBy(How = How.CssSelector, Using = "div > div.chdozwg > div:nth-child(1)")]
+        private IWebElement checkFirstDate;
+
+        [FindsBy(How = How.CssSelector, Using = "div > div.chdozwg > div:nth-child(3)")]
+        private IWebElement checkSecondDate;
+
+        [FindsBy(How = How.CssSelector, Using = "div.v1ykbue4")]
+        private IWebElement whenTab;
+
         [FindsBy(How = How.Id, Using = "bigsearch-query-location-input")]
         private IWebElement inputWhere;
 
@@ -31,6 +43,15 @@
 
         [FindsBy(How = How.Id, Using = "bigsearch-query-location-suggestion-0")]
         private IWebElement firstOption;
+
+        [FindsBy(How = How.Id, Using = "tab--tabs--1")]
+        private IWebElement btnFlexible;
+
+        [FindsBy(How = How.Id, Using = "tab--tabs--0")]
+        private IWebElement btnChooseDates;
+
+        [FindsBy(How = How.Id, Using = "flexible_trip_lengths-weekend_trip")]
+        private IWebElement btnWeekend;
         #endregion
 
         #region Actions
@@ -42,6 +63,21 @@
         public void ClickAnywhereButton()
         {
             btnAnywhere.Click();
+        }
+
+        public void ClickFlexibleButton()
+        {
+            btnFlexible.Click();
+        }
+
+        public void ClickChooseDatesButton()
+        {
+            btnChooseDates.Click();
+        }
+
+        public void ClickWeekendButton()
+        {
+            btnWeekend.Click();
         }
 
         public void SelectDestination()
@@ -57,6 +93,53 @@
         public void CheckStaysIsSelected()
         {
             btnStays.GetAttribute("aria-selected").Contains("true");
+        }
+
+        public string GetCheckInValue()
+        {
+            return checkFirstDate.Text;
+        }
+
+        public string GetCheckOutValue()
+        {
+            return checkSecondDate.Text;
+        }
+
+        public string GetWhenTabValue()
+        {
+            return whenTab.Text;
+        }
+
+        public void SelectCheckInDate()
+        {
+            List<IWebElement> tableContent = new List<IWebElement>(driver.FindElements(By.XPath("//table[@class='_cvkwaj']//td")));
+
+            foreach (IWebElement ele in tableContent)
+            {
+                string date = ele.Text;
+
+                if (date.Equals(DateTime.UtcNow.Day.ToString()))
+                {
+                    ele.Click();
+                    break;
+                }
+            }
+        }
+
+        public void SelectCheckOutDate()
+        {
+            List<IWebElement> tableContent = new List<IWebElement>(driver.FindElements(By.XPath("//table[@class='_cvkwaj']//td")));
+
+            foreach (IWebElement ele in tableContent)
+            {
+                string date = ele.Text;
+
+                if (date.Equals(DateTime.UtcNow.AddDays(4).Day.ToString()))
+                {
+                    ele.Click();
+                    break;
+                }
+            }
         }
         #endregion
     }
